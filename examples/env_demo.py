a9a97c3532567cc68598ba29ed89efb47210dc01
Jay Surse <165507454+jaysurse@users.noreply.github.com>
2026-01-20 12:59:17 +0530

    Refactor env_demo.py by removing shebang
    Removed shebang line and updated docstring.

--- examples/env_demo.py
+++ examples/env_demo.py
@@ -1,6 +1,5 @@
-from __future__ import annotations
 
-#!/usr/bin/env python3
+
 """
 Environment Variable Manager Demo
 
@@ -16,6 +15,7 @@ of Cortex, including:
 Run this script to see the env manager in action:
     python examples/env_demo.py
 """
+from __future__ import annotations
 
 import os
 import sys
